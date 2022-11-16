f275ef3f51c966adc67f1a9e4750238405e29caa
Nana kadode <amar.nana5048@gmail.com>
2022-11-17 02:18:13 +0900

    login succeeded

--- src/main/java/com/example/demo/entity/Users.java
+++ src/main/java/com/example/demo/entity/Users.java
@@ -1,61 +1,61 @@
-package com.example.demo.entity;
-
-//import javax.persistence.Entity;
-//import javax.persistence.Table;
+//package com.example.demo.entity;
 //
-//@Table(name="users")
-//@Entity
-public class Users {
-
-	private int id;
-	private String username;
-	private String password;
-	private String email;
-//	private boolean enabled;
-//	private String authorityId;
-//	private String tempkey;
-	
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-//	public boolean isEnabled() {
-//		return enabled;
+////import javax.persistence.Entity;
+////import javax.persistence.Table;
+////
+////@Table(name="users")
+////@Entity
+//public class Users {
+//
+//	private int id;
+//	private String username;
+//	private String password;
+//	private String email;
+////	private boolean enabled;
+////	private String authorityId;
+////	private String tempkey;
+//	
+//	
+//	public int getId() {
+//		return id;
 //	}
-//	public void setEnabled(boolean enabled) {
-//		this.enabled = enabled;
+//	public void setId(int id) {
+//		this.id = id;
 //	}
-//	public String getAuthorityId() {
-//		return authorityId;
+//	public String getUsername() {
+//		return username;
 //	}
-//	public void setAuthorityId(String authorityId) {
-//		this.authorityId = authorityId;
+//	public void setUsername(String username) {
+//		this.username = username;
 //	}
-//	public String getTempkey() {
-//		return tempkey;
+//	public String getPassword() {
+//		return password;
 //	}
-//	public void setTempkey(String tempkey) {
-//		this.tempkey = tempkey;
+//	public void setPassword(String password) {
+//		this.password = password;
 //	}
-}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+////	public boolean isEnabled() {
+////		return enabled;
+////	}
+////	public void setEnabled(boolean enabled) {
+////		this.enabled = enabled;
+////	}
+////	public String getAuthorityId() {
+////		return authorityId;
+////	}
+////	public void setAuthorityId(String authorityId) {
+////		this.authorityId = authorityId;
+////	}
+////	public String getTempkey() {
+////		return tempkey;
+////	}
+////	public void setTempkey(String tempkey) {
+////		this.tempkey = tempkey;
+////	}
+//}
